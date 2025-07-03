2d7168f9faecb0832f7d962b49623194047319c6
charan684 <charan.y@scaleorange.com>
2025-07-03 16:59:08 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -195,7 +195,12 @@ export const notifyUser = (message) => {
     io.to(userSocketId).emit("ride-booked", message);
   }
 };
-app.use(cors());
+app.use(cors({
+  origin: "https://ride-burc.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
